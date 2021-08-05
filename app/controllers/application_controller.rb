cd48fb2f19f2b6653655d4e0f6773aab2fae33dd
Dan Beasley <dannyleebeasley@gmail.com>
2021-08-05 12:03:21 -0500

    fixed seeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,27 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  get "/" do
-    menu_items = MenuItem.all
-    menu_items.to_json
-  end
+  # get "/" do
+  #   menu_items = MenuItem.all
+  #   menu_items.to_json
+  # end
 
   # get "/menu" do
+  #   menu_items = MenuItem.all
+  #   menu_items.to_json
   # end
 
   # get "/order" do
-  # end
+  #   new_order = OrderItem.all
+  #   new_order.to_json
+  # # end
 
   # get "/orderhistory" do
   # end
 
+  # post '/order' do
+    
+  # end
 
 
 end
